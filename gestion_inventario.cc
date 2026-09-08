f8d20311827a7a943d0995057250a560e52e6f8c
juanjo-guerra <00079926@clases.edu.sv>
2026-09-08 15:19:35 -0600

    feat: agregar función IngresarStock
    Se agregó la función  con validaciones para garantizar el ingreso correcto del stock.

--- gestion_inventario.cc
+++ gestion_inventario.cc
@@ -1,32 +1,17 @@
 #include <iostream>
 
-int main() {
-    return 0;
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-  //Funcion de Juan José Guerra Aquino
+//Funcion de Juan José Guerra Aquino
     void IngresarStock(int& stock, int cantidad) {
         if (cantidad<0)
         {
-            std::cout << "No se puede ingresar una cantidad negativa de stock." << std::endl;
+            std::cout << "No se puede ingresar una cantidad negativa de stock" << "\n";
         }
         else
         {
             stock += cantidad;
-            std::cout << "Stock ingresado correctamente. Nuevo stock: " << stock << std::endl;
+            std::cout << "Stock ingresado correctamente. Nuevo stock: " << stock << "\n";
         }
         
     }
